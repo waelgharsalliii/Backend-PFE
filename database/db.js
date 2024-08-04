fad068da89af5b4ea7d0a32fed386b6428eddbc2
waelgharsalliii <95108908+waelgharsalliii@users.noreply.github.com>
2024-08-04 23:38:38 +0100

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://admin:wael01234@mongo:27017/catch-db?authSource=admin';
+const MONGO_URI = process.env.MONGO_URI || '127.0.0.1:27017/pi';
 
 const connectDB = async () => {
   try {
