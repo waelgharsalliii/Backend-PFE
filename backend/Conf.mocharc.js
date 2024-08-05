775327dca1dc18aff2bfb52c91984cb6615f9bfc
waelgharsalliii <95108908+waelgharsalliii@users.noreply.github.com>
2024-08-05 16:02:56 +0100

    Delete backend directory

--- backend/Conf.mocharc.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    recursive: true,
-    spec: 'tests/**/*.js' ////// Replace with the pattern matching your test files
-  };
-  
--- backend/routes/paymentRoute.js
+++
@@ -1,6 +0,0 @@
-const express = require ('express');
-const Router = express.Router();
-const {Add,Verify}=require ("../routes/payment");
-Router.post('payment' ,Add);
-Router.post('payment/:id' ,Verify);
-module.exports = Router;
